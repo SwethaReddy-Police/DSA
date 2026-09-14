f8f9355b5ddf748517e5f902923dd00c32ba3728
SwethaReddy-Police <policeswethareddy16@gmail.com>
2026-09-14 19:06:35 +0530

    Time: 6 ms (73.45%) | Memory: 47.3 MB (11.20%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        char arr[]=s.toCharArray();
+        int i=0;
+        int j=arr.length-1;
+        while(i<j)
+        {
+            if(arr[i]==arr[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                if(arr[i]<arr[j])
+                {
+                    arr[j]=arr[i];
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    arr[i]=arr[j];
+                    i++;
+                    j--;
+                }
+            }
+        }
+        return new String(arr);
+        
+    }
+}
