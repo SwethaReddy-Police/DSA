a045bbb9904d0777083c86d4806bb40f4bc647a3
SwethaReddy-Police <policeswethareddy16@gmail.com>
2026-09-14 21:47:50 +0530

    Time: 5 ms (3.33%) | Memory: 44 MB (6.04%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+
+        String s1 = "";
+        String t1 = "";
+
+        // Process s
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '#') {
+                s1 = s1 + s.charAt(i);
+            }
+            else if (s1.length() > 0) {
+                s1 = s1.substring(0, s1.length() - 1);
+            }
+        }
+
+        // Process t
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) != '#') {
+                t1 = t1 + t.charAt(i);
+            }
+            else if (t1.length() > 0) {
+                t1 = t1.substring(0, t1.length() - 1);
+            }
+        }
+
+        return s1.equals(t1);
+    }
+}
