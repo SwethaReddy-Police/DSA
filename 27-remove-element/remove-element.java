7639532287063e0251c5243e65687f94546c542f
SwethaReddy-Police <policeswethareddy16@gmail.com>
2026-09-18 15:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (78.75%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int j=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=val)
+            {
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+     return j;   
+    }
+}
